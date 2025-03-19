489b9772f55349df8bd8b9de28d5a4cc2bba6f3b
MercySylver <mercyben405@gmail.com>
2025-03-19 15:23:47 +0100

    primitive type

--- PrintMethods.java
+++ PrintMethods.java
@@ -3,12 +3,10 @@ public class PrintMethods{
 		//Using the print()
 		System.out.print("\"Learning Java Is Fun\"\n");
 		System.out.print(" Hey, come and learn Java with me");
-		
 		//Using the println()
 		System.out.println();
 		System.out.println("I want to tell you that Java is different from Javascript");
 		System.out.println("\t this is the indented text"); 
-		
 		//Using the printf()
 		System.out.printf("My name is %s and i love %s%n","John","Java");
 		System.out.printf("I am living in No.%d kaduna street%n",1);
